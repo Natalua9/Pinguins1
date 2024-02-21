1475d8e60718105581eb4e0a28bc9f6b44986128
Natalua9 <148848234+Natalua9@users.noreply.github.com>
2024-02-21 12:02:26 +0500

    Add files via upload
    добавление поиска

--- mironovapinguins/header.php
+++ mironovapinguins/header.php
@@ -1,59 +1,67 @@
-<?php
-include "Connect.php";
-$query_get_category = "select * from categories";
-$categories = mysqli_fetch_all(mysqli_query($con, $query_get_category));
-?>
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Page Title</title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='css/style.css'>
-
-    <script src='main.js'></script>
-</head>
-
-<body>
-    <header>
-        <div class="nav-header">
-            <h2>Разделы</h2>
-            <div class="searcch-block">
-                <img src="imeges/Search.svg" alt="" class="poisc_img">
-                <form id="searchForm">
-    <input type="text" id="searchInput" placeholder="Поиск...">
-    <button type="button" onclick="searchNews()">Искать</button>
-</form>
-            </div>
-
-            <div class="vhod">
-                <img src="imeges/1.svg" alt="" class="poisc_img">
-                <a href="auto.php">Вход</a>/
-                <a href="reg.php">Регистрация</a>
-
-            </div>
-        </div>
-        <div class="text-name">
-            <h1 class="namePost1"><a href="/">Пингвины</a></h1>
-            <h3>Понедельник, Январь 1, 2018</h3>
-            <div class="pogoda">
-                <img src="imeges/Sun.svg" alt="" class="poisc_img">
-                <h3>-23°C</h3>
-            </div>
-        </div>
-    </header>
-    <main>
-        <div class="text-main">
-            
-            <?php
-            foreach ($categories as $category) {
-                echo "<li><a   href='/?cat=$category[0]'>$category[1]</a></li>";
-            }
-
-            ?>
-
-        </div>
-        
-    </main>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"  
+    integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="  
+    crossorigin="anonymous" referrerpolicy="no-referrer"> 
+</script> 
+<?php 
+include "Connect.php"; 
+$query_get_category = "select * from categories"; 
+$categories = mysqli_fetch_all(mysqli_query($con, $query_get_category)); 
+?> 
+<!DOCTYPE html> 
+<html> 
+ 
+<head> 
+    <meta charset='utf-8'> 
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'> 
+    <title>Page Title</title> 
+    <meta name='viewport' content='width=device-width, initial-scale=1'> 
+    <link rel='stylesheet' type='text/css' media='screen' href='css/style.css'> 
+ 
+    <script src='main.js'></script> 
+</head> 
+ 
+<body> 
+    <header> 
+        <div class="nav-header"> 
+            <h2>Разделы</h2> 
+            <div class="searcch-block"> 
+                <img src="imeges/Search.svg" alt="" class="poisc_img"> 
+                <form id="search-form" action="/" method="get"> 
+                <input type="search" id='search' name="search" placeholder="Поиск"> 
+            </form> 
+            </div> 
+ 
+            <div class="vhod"> 
+                <img src="imeges/1.svg" alt="" class="poisc_img"> 
+                <a href="auto.php">Вход</a>/ 
+                <a href="reg.php">Регистрация</a> 
+ 
+            </div> 
+        </div> 
+        <div class="text-name"> 
+            <h1 class="namePost1"><a href="/">Пингвины</a></h1> 
+            <h3>Понедельник, Январь 1, 2018</h3> 
+            <div class="pogoda"> 
+                <img src="imeges/Sun.svg" alt="" class="poisc_img"> 
+                <h3>-23°C</h3> 
+            </div> 
+        </div> 
+    </header> 
+    <main> 
+        <div class="text-main"> 
+             
+            <?php 
+            foreach ($categories as $category) { 
+                echo "<li><a   href='/?cat=$category[0]'>$category[1]</a></li>"; 
+            } 
+            ?> 
+        </div> 
+    </main> 
+ 
+    <script>  
+        $('#search-form').on ('keyup', function (e)){  
+            <?php   
+                $searching = isset($_GET['search'])? $_GET['search']: false;   
+                ?>  
+        }  
+    </script>
--- mironovapinguins/index.php
+++ mironovapinguins/index.php
@@ -82,83 +82,71 @@
 <!-- </body>
 </html> -->
 
-<?php
-include "connect.php"; //выражение include включает и выполняет указанный файл
-
-$query_get_category = "select * from categories"; 
-
-$categories = mysqli_fetch_all(mysqli_query($con, $query_get_category));
-
-include "header.php";
-
-$news = mysqli_query($con, "select * from news");
-
-$id_cat = isset($_GET['cat'])? $_GET['cat']:false;
-$sort = isset($_GET['sort'])?$_GET['sort']:false;    
-$category = isset($_GET['category'])?$_GET['category']:false;  
-
-$query_newcat = "SELECT * FROM news";
-
-if ($id_cat) {
-    $query_newcat .= " WHERE category_id = '$id_cat'";
-}
-else if ($category) { $id_cat = $category;
-    $query_newcat .= " WHERE category_id = '$id_cat'";}
-
-if ($sort) {
-    $sort_order = ($sort == 'asc') ? 'ASC' : 'DESC';
-    $query_newcat .= " ORDER BY publish_date $sort_order";
-}
-
-$result = mysqli_query($con, $query_newcat);
-
-
-?>
-
-
-<form action = "" method="get">
-<label for="sort">Сортировать по дате:</label>
-    <select name="sort" id="sort">
-        <option value="">Без сортировки</option>
-        <option value="asc" <?= ($sort and $sort == "asc") ? "selected" : ""; ?>>По возрастанию</option>
-        <option value="desc" <?= ($sort and $sort == "desc") ? "selected" : ""; ?>>По убыванию</option>
-    </select>
-    <input type="hidden" value="<?=$id_cat?>" name='category'>
-    <button type="submit">Применить</button>
-</form>
-    <main1>
-        <div class="cards">
-            <?php
-        if (mysqli_num_rows($result) == 0) {
-            echo "Нет новостей";
-        } else {
-            while ($new = mysqli_fetch_assoc($result)) { 
-                echo "<div class='card'>"; 
-                $new_id = $new['news_id']; 
-                $new_date = $new['publish_date']; 
-                echo "<img src='imeges/news/" . $new['image']. "'>"; 
-                echo "<a href='oneNew.php?new=$new_id'>" . $new['title'] . "</a>"; 
-                echo "</div>"; 
-            }
-        }
-            ?>
-        </div>
-    </main1>
-    
-</body>
+<?php 
+include "connect.php"; //выражение include включает и выполняет указанный файл 
+ 
+$query_get_category = "select * from categories";  
+ 
+$categories = mysqli_fetch_all(mysqli_query($con, $query_get_category)); 
+ 
+include "header.php"; 
+ 
+$news = mysqli_query($con, "select * from news"); 
+ 
+$id_cat = isset($_GET['cat'])? $_GET['cat']:false; 
+$sort = isset($_GET['sort'])?$_GET['sort']:false;     
+$category = isset($_GET['category'])?$_GET['category']:false;   
+ 
+$query_newcat = "SELECT * FROM news"; 
+ 
+if ($id_cat) { 
+    $query_newcat .= " WHERE category_id = '$id_cat'"; 
+} 
+else if ($category) { $id_cat = $category; 
+    $query_newcat .= " WHERE category_id = '$id_cat'";} 
+ 
+if ($sort) { 
+    $sort_order = ($sort == 'asc') ? 'ASC' : 'DESC'; 
+    $query_newcat .= " ORDER BY publish_date $sort_order"; 
+} 
+if($searching){  
+    $query_newcat = "SELECT * FROM news WHERE title LIKE '%$searching%'";   
+} 
+$result = mysqli_query($con, $query_newcat); 
+$search = isset($_GET["search"]) ? $_GET["search"] : false; 
+ 
+?> 
+ 
+ 
+<form action = "" method="get"> 
+<label for="sort">Сортировать по дате:</label> 
+    <select name="sort" id="sort"> 
+        <option value="">Без сортировки</option> 
+        <option value="asc" <?= ($sort and $sort == "asc") ? "selected" : ""; ?>>По возрастанию</option> 
+        <option value="desc" <?= ($sort and $sort == "desc") ? "selected" : ""; ?>>По убыванию</option> 
+    </select> 
+    <input type="hidden" value="<?=$id_cat?>" name='category'> 
+    <button type="submit">Применить</button> 
+</form> 
+    <main1> 
+        <div class="cards"> 
+            <?php 
+             
+        if (mysqli_num_rows($result) == 0) { 
+            echo "Нет новостей"; 
+        } else { 
+            while ($new = mysqli_fetch_assoc($result)) {  
+                echo "<div class='card'>";  
+                $new_id = $new['news_id'];  
+                $new_date = $new['publish_date'];  
+                echo "<img src='imeges/news/" . $new['image']. "'>";  
+                echo "<a href='oneNew.php?new=$new_id'>" . $new['title'] . "</a>";  
+                echo "</div>";  
+            } 
+        } 
+            ?> 
+        </div> 
+    </main1> 
+     
+</body> 
 </html>
-<script>
-function searchNews() {
-    var input = document.getElementById('searchInput').value.toLowerCase();
-    var cards = document.querySelectorAll('.card');
-
-    cards.forEach(function(card) {
-        var title = card.querySelector('a').innerText.toLowerCase();
-        if (title.includes(input)) {
-            card.style.display = 'block';
-        } else {
-            card.style.display = 'none';
-        }
-    });
-}
-</script>
